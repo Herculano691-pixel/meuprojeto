23f9f1b1cfe5c08c27e63e63101ccdde5d348d6d
EA - Breno Henrique Leão Herculano <breno_herculano@acad.unasp.edu.br>
2026-04-02 20:15:24 -0300

    explicação de versionamento

--- github.py
+++ github.py
@@ -1,2 +1,17 @@
 print("olá Breno")
 
+#Primeiro Versionamento
+
+#git init
+#git add .
+#git commit -m "mensagem"
+#git remote add origin link
+#git push -u origin main
+
+
+#Depois de ligado ao git
+
+#git status      # ver o que mudou
+#git add .       # adicionar os arquivos
+#git commit -m "mensagem explicando a mudança"
+#git push        # enviar para o GitHub
