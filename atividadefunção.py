8b421957a20be12f3e0105136b0bcbb33fc06ea0
EA - Breno Henrique Leão Herculano <breno_herculano@acad.unasp.edu.br>
2026-04-16 21:53:13 -0300

    atividades

--- atividadefunção.py
+++ atividadefunção.py
@@ -1,13 +1,18 @@
+
 def salario_novo(salario, cargo):
-    if cargo == 'Junior':
+    try:
+       if cargo == 'Junior':
         return salario * 1.05
-    if cargo == 'Pleno':
+       if cargo == 'Pleno':
         return salario * 1.10
-    if cargo == 'Senior':
+        if cargo == 'Senior':
         return salario * 1.15
     
-    print('Cargo não existe')
-    return salario
+         print('Cargo não existe')
+         return salario
 
-resultado = salario_novo(1000, 'Senior')
-print('Salário novo:', resultado)
+         resultado = salario_novo('Senior', 'Senior')
+         print('Salário novo:', resultado) 
+    except exception as e:
+         print('tem um erro:' , e)
+
--- funções.py
+++ funções.py
@@ -1,4 +1,5 @@
-def par_impar(x):
+#para criar função 'def'
+def par_impar(x): 
     y = x % 2  
     if y == 0:
         return "par"
@@ -7,3 +8,10 @@ def par_impar(x):
 
 a = par_impar(4)
 print(a)
+
+"""
+try:
+    y = [1, 2, 3]
+    print(y[3])
+except:
+    print("tem um erro")
